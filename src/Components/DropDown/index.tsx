459688a80452610161bb8da271d5926f5fcfdb22
suryagoku <gokul.surya@flyerssoft.com>
2023-01-04 19:14:24 +0530

    update PR changes

--- src/Components/DropDown/index.tsx
+++ src/Components/DropDown/index.tsx
@@ -6,28 +6,24 @@ interface dropDown {
 }
 
 const DropDown = ({ options }: dropDown) => {
-  const [value, setValue] = useState('');
-  const [show, setShow] = useState(false);
-  const showHide = () => {
-    setShow(!show);
-    console.log(show);
-  };
+  const [value, setValue] = useState<string>('');
+  const [showOptions, setShowOptions] = useState<boolean>(false);
   const changeValue = (val: string) => {
     setValue(val);
-    setShow(!show);
+    setShowOptions(!showOptions);
   };
   return (
     <div>
       <div
         className="flex items-center w-343 justify-between border-1 h-56 p-16 rounded-16 relative"
-        onClick={showHide}
+        onClick={() => setShowOptions(!showOptions)}
       >
         <span>{value === '' ? 'Account type' : value}</span>
         <img src={Vector} alt="" className="w-20 h-10"></img>
       </div>
       <div
-        style={{ display: show ? 'block' : 'none' }}
-        className="flex items-center w-343 justify-between border-1 rounded-16 mt-16 absolute bg-white z-1"
+        style={{ display: showOptions ? 'block' : 'none' }}
+        className="flex items-center w-343 justify-between border-1 rounded-16 mt-16 absolute bg-white z-index-1"
       >
         {options.map((option) => (
           <div
