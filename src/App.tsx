7b63ab5563e32f972669899881b311a10854cda1
Jaysh7 <hellojayshu@gmail.com>
2023-01-04 19:48:22 +0530

    App changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import Button from './Components/Button'
-import Hero from './Components/Header'
-
 function App() {
 
   /**
@@ -12,20 +9,9 @@ function App() {
    * 5. Children
    */
 
-  const btnClick = () => {
-    //function goes here
-  }
-  
   return (
     <div className="App">
-      <div className='flex flex-col'>
-      <Button onClick={btnClick} variant="Primary">Sign Up </Button>
-      <Button variant="secondry">Login</Button>
-      <Button variant="text">Forgot Password?</Button>
-      </div>
-      <div>
-      <Hero title="Sign Up"/>
-      </div>
+      
     </div>
   )
 }
