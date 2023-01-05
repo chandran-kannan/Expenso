02cf80d16c79014cc2edf08608ee777b6723f6da
vichu78 <vishnu.ravichandran@flyerssoft.com>
2023-01-05 15:55:36 +0530

    include attribute in input component

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -5,8 +5,8 @@ const Register = () => {
   
       <form className="mt-56px flex flex-col gap-24px h-216px ">
         <Input type="text" placeholder="Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="password" />
+        {/* <Input type="email" placeholder="Email" /> */}
+        {/* <Input type="password" placeholder="password" /> */}
       </form>
   
   );
