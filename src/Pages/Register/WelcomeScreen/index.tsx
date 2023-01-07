4ae8df6efb78fc1a0403a3d710883bf9426d547b
Jaysh7 <hellojayshu@gmail.com>
2023-01-07 17:20:40 +0530

    WelcomeScreen

--- src/Pages/Register/WelcomeScreen/index.tsx
+++ src/Pages/Register/WelcomeScreen/index.tsx
@@ -1,17 +1,20 @@
 
 import Carousel from '../../../Components/carousel';
 import Button from '../../../Components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const WelcomeScreen = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className='w-350px px-15px m-auto'>
       <div>
         <Carousel />
       </div>
       <div className='mt-33px'>
-        <Button variant="primary">Sign Up</Button>
+        <Button variant="primary" onClick={() => navigate('/signup')}>Sign Up</Button>
         <div className='mt-16px'>
-          <Button variant="secondry">Login</Button>
+          <Button variant="secondry" onClick={() => navigate('/login')}>Login</Button>
         </div>
       </div>
     </div>
