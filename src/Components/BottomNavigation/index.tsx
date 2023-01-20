55d083597c4abc19e5ff903778929ea55ab97cb4
shunmugaprasanna <shunmugaprasanna.k@flyerssoft.com>
2023-01-20 17:37:47 +0530

    Button Color changed

--- src/Components/BottomNavigation/index.tsx
+++ src/Components/BottomNavigation/index.tsx
@@ -19,52 +19,57 @@ const BottomNavigation = () => {
   };
   return (
     <div className="mt-200px flex">
-      <div className="flex justify-evenly bg-primary w-187px border-top-right-radius-38px border-top-left-radius-10px">
-        <div className="flex flex-col items-center">
-          <Home fill="#C6C6C6" className="w-24px h-24px" />
+      <div className="flex justify-evenly pointer bg-primary w-187px border-top-right-radius-18px border-top-left-radius-10px">
+        <div className="flex flex-col items-center justify-evenly">
+          <Home fill="#C6C6C6" className="w-24px" />
           <span className="fs-10px text-shade-grey font-500">Home</span>
         </div>
-        <div className="flex flex-col items-center">
-          <Piechart fill="#C6C6C6" className="w-24px h-24px" />
+        <div className="flex flex-col items-center justify-evenly">
+          <Piechart fill="#C6C6C6" className="w-24px" />
           <span className="fs-10px text-shade-grey font-500">Transaction</span>
         </div>
       </div>
 
-      <div className="border-bottom-40px border-top-radius-38px bg-primary border-top-right-radius-37px border-top-left-radius-37px">
+      <div className="border-bottom-40px border-top-radius-38px bg-primary border-top-right-radius-30px border-top-left-radius-30px">
         <button
           onClick={handleHomeButton}
-          className="border-none pointer transform-10px  bg-primary border-w-5px border-bg-secondary border-style-solid flex items-center justify-center rounded-50per w-80px h-80px"
+          className="border-none pointer transform-17px  bg-primary border-w-5px 
+          border-bg-white border-style-solid flex items-center justify-center rounded-50per w-80px h-80px"
         >
           {changeIcon === true ? (
-            <Delet fill="white" className="w-24px h-24px" />
+            <Delet fill="white" className="w-24px" />
           ) : (
-            <Add className="w-24px h-24px" />
+            <Add fill="white" className="w-24px" />
           )}
           {changeIcon && (
             <div className="flex absolute b-95px l-49px gap-55px">
-              <button className="border-none pointer p-12px flex items-center justify-center  bg-green  rounded-50per  w-56px h-56px">
-                <Income fill="white" className="h-24px w-24px" />
+             <div className="absolute l-130px b-5px">
+             <button className="border-none pointer p-12px flex items-center justify-center  bg-green  rounded-50per  w-56px h-56px">
+                <Income fill="white" className="w-24px" />
               </button>
+             </div>
               <div className="absolute b-67px l-50px">
                 <button className="border-none pointer p-12px flex items-center justify-center bg-blue rounded-50per w-56px h-56px">
-                  <MoneyExchange fill="white" className="h-24px w-24px" />
+                  <MoneyExchange fill="white" className="w-24px" />
                 </button>
               </div>
-              <button className="border-none pointer bg-red flex items-center justify-center rounded-50per w-56px h-56px ">
-                <Outcome fill="white" className="h-24px w-24px" />
+             <div className="absolute l-20px b-5px">
+             <button className="border-none pointer bg-red flex items-center justify-center rounded-50per w-56px h-56px ">
+                <Outcome fill="white" className="w-24px" />
               </button>
+             </div>
             </div>
           )}
         </button>
       </div>
 
-      <div className="flex justify-evenly  bg-primary w-187px border-top-right-radius-10px border-top-left-radius-38px">
-        <div className="flex flex-col items-center">
-          <Transaction fill="#C6C6C6" className="w-24px h-24px" />
-          <span className="fs-10px text-shade-grey font-500">Home</span>
+      <div className="flex justify-evenly pointer  bg-primary w-187px border-top-right-radius-10px border-top-left-radius-18px ">
+        <div className="flex flex-col items-center justify-evenly">
+          <Transaction fill="#C6C6C6" className="w-24px " />
+          <span className="fs-10px text-shade-grey font-500">Budget</span>
         </div>
-        <div className="flex flex-col items-center">
-          <User fill="#C6C6C6" className="w-24px h-24px" />
+        <div className="flex flex-col items-center justify-evenly">
+          <User fill="#C6C6C6" className="w-24px" />
           <span className="fs-10px text-shade-grey font-500">Transaction</span>
         </div>
       </div>
