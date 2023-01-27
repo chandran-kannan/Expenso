311d0e10c86f4a3d4c1683757b20ef3cc140d9e0
Raja rathinam v <raja.rathinam@flyerssoft.com>
2023-01-27 13:57:42 +0530

    update the tab content using props method

--- src/Pages/HomeScreen/index.tsx
+++ src/Pages/HomeScreen/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Tab from "../../Components/Tab";
 
-const HomeScreen = ()=>
+const HomeScreen = () =>
 {
     return(
         <React.Fragment>
         <Tab />
-         <
+       
         </React.Fragment>
     )
 }
